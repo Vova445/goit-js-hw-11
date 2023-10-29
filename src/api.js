49a730f447373ef5d321f62c9b0cbb9d5f4ea7d8
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-10-29 23:02:40 +0200

    fixed

--- src/api.js
+++ src/api.js
@@ -5,12 +5,15 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '40357504-569de062b9af68d468be4e854';
 
 export async function fetchData(searchQuery, page) {
-  try {
-    const res = await axios.get(
+  return axios
+    .get(
       `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-    );
-    return res.data;
-  } catch (error) {
-    Notiflix.Notify.failure('Something went wrong in "fetchData"');
-  }
+    )
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      Notiflix.Notify.failure('Something went wrong in "fetchData"');
+      throw error; 
+    });
 }
